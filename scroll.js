9859cfec930b76bfc13befdfdaadcf6d24a63bf0
unknown <katya-alex31@mail.ru>
2022-11-14 12:09:47 +0400

    serviscom

---
+++ scroll.js
@@ -0,0 +1,11 @@
+function updateScroll() {
+    if (window.scrollY > 0) {
+      document.querySelector('header').classList.add('header__scrolled');
+    } else {
+      document.querySelector('header').classList.remove('header__scrolled');
+    };
+   
+  }
+  
+  window.addEventListener('scroll', updateScroll);
+  
